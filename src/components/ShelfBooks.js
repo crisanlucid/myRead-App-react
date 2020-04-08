be213cac47d0a5b87e97be8c1e3b7129f9065de5
crisanlucid <crisan.lucid@yahoo.com>
2020-04-09 00:18:19 +0200

    feat: Create Components: ShelfBook, Book - update ListBooksPage with new components - create new array of object from books based Shelf type - ShelfBook props: books, shelves, title, onUpdateBook

---
+++ src/components/ShelfBooks.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Book from './Book';
+
+const ShelfBooks = ({ books, shelves, onUpdateBook, title }) => {
+    return (
+        <div className='bookshelf'>
+            <h2 className='bookshelf-title'>{title}</h2>
+            <div className='bookshelf-books'>
+                <ol className='books-grid'>
+                    {
+                        books.map(book => (
+                            <Book book={book} key={book.title} shelves={shelves} onUpdateBook={onUpdateBook} />
+                        ))
+                    }
+
+                </ol>
+            </div>
+        </div>
+    );
+};
+
+ShelfBooks.propTypes = {
+    books: PropTypes.array.isRequired,
+    shelves: PropTypes.array.isRequired,
+    title: PropTypes.string.isRequired,
+    onUpdateBook: PropTypes.func.isRequired,
+};
+
+export default ShelfBooks;
