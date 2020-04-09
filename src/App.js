e91a781cee8cfbe08914441f5ee6825f11bd97de
crisanlucid <crisan.lucid@yahoo.com>
2020-04-09 11:36:09 +0200

    feat: SearchBooksPage Component - create class component - SearchBooksPage props: books, shelves, onSearchBook, onUpdateBook - remove static code from App.js and include in new Component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css'
 
 import { Route } from 'react-router-dom';
 import ListBooksPage from './pages/ListBooksPage';
+import SearchBooksPage from './pages/SearchBooksPage';
 
 const SHELF_LIST = [{ categ: 'currentlyReading', text: 'Currently Reading' }, { categ: 'wantToRead', text: 'Want to Read' }, { categ: 'read', text: 'Read' }];
 class BooksApp extends React.Component {
@@ -22,6 +23,10 @@ class BooksApp extends React.Component {
     console.log('parent update Book with custom Shelf...')
   }
 
+  handleSearchBook = () => {
+    console.log('parent update List of Books...')
+  }
+
   render() {
     console.log('this.state.bookList', this.state.bookList)
     console.log('this.state.shelfList', this.state.shelfList)
@@ -36,26 +41,12 @@ class BooksApp extends React.Component {
           />
         )} />
         <Route path='/search' component={() => (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <button className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</button>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author" />
-
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
+          <SearchBooksPage
+            books={bookList}
+            shelves={shelfList}
+            onUpdateBook={this.handleUpdateBook}
+            onSearchBook={this.handleSearchBook}
+          />
         )} />
 
 
