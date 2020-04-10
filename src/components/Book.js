3b44f58bdaa453f505eb6e1917e9ce61a8f2fee9
crisanlucid <crisan.lucid@yahoo.com>
2020-04-10 22:35:26 +0200

    fix: Book Component - update Book component into Statefull Component - create a Control Component for Select HTML tag - create State shelf variable updating from parent book.shelf - call the callback for HandleUpdateBook

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,42 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { render } from 'react-dom';
 
-const Book = ({ book, shelves, onUpdateBook }) => {
-  return (
-    <li>
-      <div className='book'>
-        <div className='book-top'>
-          <div
-            className='book-cover'
-            style={{
-              width: 128,
-              height: 193,
-              backgroundImage: `url("${
-                book.imageLinks ? book.imageLinks.thumbnail : ''
-              }")`,
-            }}></div>
-          <div className='book-shelf-changer'>
-            <select
-              onChange={(event) => onUpdateBook(event, book)}
-              value={book.shelf}>
-              <option value='move' disabled>
-                Move to...
-              </option>
-              {shelves.map((shelf) => (
-                <option key={shelf.categ} value={shelf.categ}>
-                  {shelf.text}
+class Book extends Component {
+  state = {
+    shelf: this.props.book.shelf,
+  };
+
+  handleUpdateBook = (event, book) => {
+    this.props.onUpdateBook(event, book);
+  };
+
+  render() {
+    const { book, shelves } = this.props;
+    return (
+      <li>
+        <div className='book'>
+          <div className='book-top'>
+            <div
+              className='book-cover'
+              style={{
+                width: 128,
+                height: 193,
+                backgroundImage: `url("${
+                  book.imageLinks ? book.imageLinks.thumbnail : ''
+                }")`,
+              }}></div>
+            <div className='book-shelf-changer'>
+              <select
+                value={this.state.shelf}
+                onChange={(event) => this.handleUpdateBook(event, book)}>
+                <option value='move' disabled>
+                  Move to...
                 </option>
-              ))}
-              <option value='none'>None</option>
-            </select>
+                {shelves.map((shelf) => (
+                  <option key={shelf.categ} value={shelf.categ}>
+                    {shelf.text}
+                  </option>
+                ))}
+                <option value='none'>None</option>
+              </select>
+            </div>
           </div>
+          <div className='book-title'>{book.title}</div>
+          <div className='book-authors'>{book.authors}</div>
         </div>
-        <div className='book-title'>{book.title}</div>
-        <div className='book-authors'>{book.authors}</div>
-      </div>
-    </li>
-  );
-};
+      </li>
+    );
+  }
+}
 
 Book.propsTypes = {
   book: PropTypes.object.isRequired,
