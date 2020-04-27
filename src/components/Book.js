28d3a67a96af99a3b04a3faa660194038ee8aff8
crisanlucid <crisan.lucid@yahoo.com>
2020-04-27 23:27:47 +0200

    fix: Separate Authors by comma

--- src/components/Book.js
+++ src/components/Book.js
@@ -54,7 +54,11 @@ class Book extends Component {
             </div>
           </div>
           <div className='book-title'>{book.title}</div>
-          {book.authors && <div className='book-authors'>{book.authors}</div>}
+          {book.authors && (
+            <div className='book-authors'>
+              {Array.isArray(book.authors) ? book.authors.join(', ') : ''}
+            </div>
+          )}
         </div>
       </li>
     );
